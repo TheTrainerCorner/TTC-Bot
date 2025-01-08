c3158f297ee9397dc47f350dced5729aa7cb35a8
Cody Spratford <koreanpanda345@gmail.com>
2025-01-08 09:18:07 -0800

    Update elo.ts

--- src/client/modules/monitors/elo.ts
+++ src/client/modules/monitors/elo.ts
@@ -9,7 +9,6 @@ export default class EloMonitor extends BaseMonitor {
 
     public async invoke(analyzer: Analyzer) {
         const data = analyzer.data;
-        console.log('Success');
 
         let player1 = await Player.findOne({ showdownUsername: data.p1.username});
         let player2 = await Player.findOne({ showdownUsername: data.p2.username});
@@ -36,26 +35,35 @@ export default class EloMonitor extends BaseMonitor {
         const step2 = Math.floor(Math.round(step1 / 400));
         const step3 = Math.floor(Math.round(Math.pow(10, (step2 + 1))));
         const expected = Math.floor(Math.round(1 / step3));
+        const ka = 20;
+        const kb = 20;
 
-        const ka = a <= 1000 ? 40 :
-                    a >= 1000 && a <= 1200 ? 30 :
-                     a >= 1200 && a <= 1600 ? 20 :
-                      a >= 1600 ? 10 : 40;
-        const kb = b <= 1000 ? 40 :
-                    b >= 1000 && b <= 1200 ? 30 :
-                     b >= 1200 && b <= 1600 ? 20 :
-                      b >= 1600 ? 10 : 40;
+        // const ka = a <= 1000 ? 40 :
+        //             a >= 1000 && a <= 1200 ? 30 :
+        //              a >= 1200 && a <= 1600 ? 20 :
+        //               a >= 1600 ? 10 : 40;
+        // const kb = b <= 1000 ? 40 :
+        //             b >= 1000 && b <= 1200 ? 30 :
+        //              b >= 1200 && b <= 1600 ? 20 :
+        //               b >= 1600 ? 10 : 40;
 
         const aScore = Math.floor(Math.round(ka * ((data.winner === data.p1.username ? 1 : 0) - expected)));
         const bScore = Math.floor(Math.round(kb * ((data.winner === data.p2.username ? 1 : 0) - expected)));
+
+        // const aScore = Math.floor(Math.round(ka * ((data.winner === data.p1.username ? 1 : 0) - expected)));
+        // const bScore = Math.floor(Math.round(kb * ((data.winner === data.p2.username ? 1 : 0) - expected)));
         
         player1.elo += aScore;
-        if (data.winner === data.p1.username) player1.wins += 1;
-        else player1.losses += 1;
-        player1.matchesPlayed += 1;
+        player1.matchesPlayed++;
         player2.elo += bScore;
-        if (data.winner === data.p2.username) player2.wins += 1;
-        else player2.losses += 1;
+        player2.matchesPlayed++;
+        if (data.winner === data.p1.username) {
+            player1.wins++;
+            player2.losses++;
+        } else if (data.winner === data.p1.username) {
+            player1.losses++;
+            player2.wins++;
+        }
 
         await player1.save();
         await player2.save();
